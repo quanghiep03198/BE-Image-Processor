35b59625972e0d27bbcc3d5339f47fded262179e
quanghiep03198 <quanghiep03198@gmail.com>
2026-05-04 16:36:21 +0700

    feat: implement authentication and image handling routes with MongoDB integration

---
+++ databases/__init__.py
@@ -0,0 +1,3 @@
+from databases.mongo import mongo_client, db
+
+__all__ = ["mongo_client", "db"]
---
+++ routes/__init__.py
@@ -0,0 +1,7 @@
+from routes.image import router as img_router
+from routes.ws_preview import router as ws_router
+from routes.auth import router as auth_router
+
+# image_router = __img_router
+# ws_router = __ws_router
+__all__ = ["img_router", "ws_router", "auth_router"]
