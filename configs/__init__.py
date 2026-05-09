4070fded65237f5981b20702f234d3d10ac24849
quanghiep03198 <quanghiep03198@gmail.com>
2026-05-10 00:27:34 +0700

    refactor: update env function to use Literal for key type

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,13 +1,12 @@
-from dotenv import load_dotenv, dotenv_values
-from typing import Callable, Any
-from os import path
-from pathlib import Path
+from typing import Any, Callable, Literal
+
+from dotenv import dotenv_values, load_dotenv
 
 load_dotenv()
 
 
 def env(
-    key: str,
+    key: Literal["JWT_SECRET_KEY", "MONGO_URI", "SALT_ROUNDS"],
     default=None,
     serializer: Callable[[str], Any] | None = None,
 ):
