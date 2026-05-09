96b64da8cb96d1f1ce44fc94529183a9bf818b59
quanghiep03198 <quanghiep03198@gmail.com>
2026-05-10 00:27:48 +0700

    feat: refactor MongoDB connection handling with async support and Beanie initialization

--- databases/__init__.py
+++
@@ -1,3 +0,0 @@
-from databases.mongo import mongo_client, db
-
-__all__ = ["mongo_client", "db"]
--- databases/mongo.py
+++ databases/mongo.py
@@ -1,22 +1,51 @@
-from pymongo import MongoClient
-from fastapi.logger import logger
+from asyncio import *
 
-global mongo_client, db
-try:
-    mongo_client = MongoClient(
-        host=[
-            "127.0.0.1:27017",
-            "127.0.0.1:27018",
-            "127.0.0.1:27019",
-        ],
-        connect=True,
-        replicaSet="rs0",
-        serverSelectionTimeoutMS=5000,
-        connectTimeoutMS=5000,
-    )
-    db = mongo_client.get_database(name="image_processor")
-    logger.info("Connected to MongoDB")
-except Exception as e:
-    logger.error(f"Failed to connect to MongoDB: {e}")
-    mongo_client = None
-    db = None
+# import asyncio
+from typing import Optional
+
+from beanie import init_beanie
+from fastapi.logger import logger
+from pymongo import AsyncMongoClient
+
+from configs import env
+from models.image import ImageModel
+from models.revoke_token import RevokeTokenModel
+from models.user import UserModel
+
+global db
+
+
+class Database:
+    client: Optional[AsyncMongoClient] = None
+
+
+db = Database()
+
+
+async def connect_database():
+    try:
+        db.client = AsyncMongoClient(env("MONGO_URI"))
+        logger.info("Connected to MongoDB")
+        # database = db.client["image_processor"]
+        # * Initialize Beanie with the database and document models
+        await init_beanie(
+            database=db.client.get_database("image_processor"),
+            document_models=[UserModel, RevokeTokenModel, ImageModel],
+        )
+        logger.info("Beanie initialized with MongoDB")
+    except Exception as e:
+        logger.error(f"Failed to connect to MongoDB: {e.args}")
+
+
+# asyncio.run(connect_database())
+
+
+async def close_database() -> None:
+    if db.client:
+        await db.client.close()
+        logger.info("MongoDB connection closed")
+
+
+def get_database(name="image_processor"):
+    if db.client:
+        return db.client.get_database(name)
