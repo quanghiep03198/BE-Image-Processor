a5592e69c9319f32e20297b14f70414dac69ad78
quanghiep03198 <quanghiep03198@gmail.com>
2026-05-10 00:27:55 +0700

    feat: add custom AppException class for error handling with logging

---
+++ exceptions/__init__.py
@@ -0,0 +1,9 @@
+from fastapi.logger import logger
+
+
+class AppException(Exception):
+    def __init__(self, status_code: int, message: str):
+        self.status_code = status_code
+        self.message = message
+
+        logger.error(f"AppException: {status_code} - {message}")
